ddbaec15e66324ab80d7d95d81be5fe43ab619ac
Ilya Dzhubansky <starkyru@gmail.com>
2022-05-26 21:15:23 -0400

    Enable redux devtools

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { createRootReducer } from './mainReducer';
 
-const store = configureStore({ reducer: createRootReducer() });
+const store = configureStore({ reducer: createRootReducer(), devTools: true });
 
 export type AppDispatch = typeof store.dispatch;
 export type AppState = ReturnType<typeof store.getState>;
