05eb6b4f5dd5d82c28c96032d7bb388ea1c68a28
Ilya Dzhubansky <starkyru@gmail.com>
2022-05-31 12:50:12 -0400

    Fixed: strange issue with metro config, animation end handler

--- src/components/HeaderTitle/HeaderTitle.tsx
+++ src/components/HeaderTitle/HeaderTitle.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { TextProps } from 'react-native';
-import Logo from '../../assets/shop-logo-us.svg';
+import Logo from '../../assets/shop-logo-us';
 
 export const HeaderTitle: React.FC<TextProps> = () => {
   return <Logo height={50} width="100%" />;
